25c59e7f3621ff80386bc458df2cc8196f7a1b7b
tulsee-agrawal <tulsee.bargarh@gmail.com>
2026-03-22 23:16:46 +0530

    Code dUC7 - applied conversion logic

--- js/script.js
+++ js/script.js
@@ -127,3 +127,23 @@ loadHistory();
     }
 
 });
+
+export function applyConversion(value, convObj) {
+
+    if (!Number.isFinite(value)) {
+        throw new Error("Invalid number");
+    }
+
+    if (convObj.factor !== null) {
+        return parseFloat((value * convObj.factor).toFixed(6));
+    }
+
+    try {
+        const expr = convObj.formula.replace("x", value);
+        const result = eval(expr);
+
+        return parseFloat(result.toFixed(6));
+    } catch (error) {
+        throw new Error("Bad formula");
+    }
+}
