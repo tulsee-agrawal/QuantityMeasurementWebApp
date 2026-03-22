37aaedc6d90691fb50c5e63e5a7744628d47bd6d
tulsee-agrawal <tulsee.bargarh@gmail.com>
2026-03-22 23:20:40 +0530

    Coded UC9- Performed arithmetic operation

--- js/script.js
+++ js/script.js
@@ -162,3 +162,28 @@ function compareValues(v1, u1, v2, u2, base1, base2) {
 
     return `${v1} ${u1} is EQUAL to ${v2} ${u2}`;
 }
+function performArithmetic(v1, v2normalised, op) {
+    if (!Number.isFinite(v1) || !Number.isFinite(v2normalised)) {
+        throw new Error("Invalid number");
+    }
+
+    switch (op) {
+        case "+":
+            return parseFloat((v1 + v2normalised).toFixed(6));
+
+        case "-":
+            return parseFloat((v1 - v2normalised).toFixed(6));
+
+        case "*":
+            return parseFloat((v1 * v2normalised).toFixed(6));
+
+        case "/":
+            if (v2normalised === 0) {
+                throw new Error("Divide by zero");
+            }
+            return parseFloat((v1 / v2normalised).toFixed(6));
+
+        default:
+            throw new Error("Unknown operator");
+    }
+}
